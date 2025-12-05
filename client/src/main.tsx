6b844135e42b0f725adb0847e366dc4459dc31a8
zazaborisovi <145875592+zazaborisovi@users.noreply.github.com>
2025-12-05 21:35:48 +0400

    .

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './context/auth.context.jsx'
+import AuthProvider from './context/auth.context.jsx'
 import { BrowserRouter } from 'react-router'
-import { LaptopProvider } from './context/laptops.context.jsx'
+import LaptopProvider from './context/laptops.context.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
